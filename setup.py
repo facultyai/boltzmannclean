9f03022d6e29d4ab777bc6273062ce2f2a2dbb9a
Scott Stevenson <scott@stevenson.io>
2018-09-13 15:55:52 +0100

    Cleanup setup.py
    * Remove commented out code
    * Refactor helper function
    * Change URL to source repository

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
-from setuptools import setup
 import os
+from setuptools import setup
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-# README = Path(__file__).parent / 'README.rst'
+def read_long_description():
+    with open(os.path.join(os.path.dirname(__file__), "README.rst")) as fp:
+        return fp.read()
 
 
 setup(
-    name='sherlockml-boltzmannclean',
-    version='0.1.2',
-    url='https://sherlockml.com',
-    author='ASI Data Science',
-    author_email='engineering@asidatascience.com',
-    description='Fills missing values in a pandas DataFrame using a Restricted'
-                ' Boltzmann Machine.',
-    license='Apache 2.0',
-    long_description=read('README.rst'),
-    py_modules=['boltzmannclean'],
-    install_requires=[
-        'pandas',
-        'numpy',
-        'scipy',
-        'scikit-learn'
-    ]
+    name="sherlockml-boltzmannclean",
+    version="0.1.2",
+    url="https://github.com/ASIDataScience/sherlockml-boltzmannclean",
+    author="ASI Data Science",
+    author_email="engineering@asidatascience.com",
+    description="Fill missing values in DataFrames with Restricted Boltzmann Machines",
+    license="Apache 2.0",
+    long_description=read_long_description(),
+    py_modules=["boltzmannclean"],
+    install_requires=["pandas", "numpy", "scipy", "scikit-learn"],
 )
